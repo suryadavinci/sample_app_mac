76911d61ed369d5183eb3b6ed1b63004e19de8e5
“Surya <suryadavinci@gmail.com>
2017-07-07 00:05:13 +0530

    from room

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world! Surya Sample App Rocks!!!!! From Home"
+    render html: "hello, world! Surya Sample App Rocks!!!!! From Home!!! FROM ROOMs"
   end
 end
