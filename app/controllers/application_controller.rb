601c863f67f063a9fcbf5a3ffe3900f84eb1e2f9
“Surya <suryadavinci@gmail.com>
2017-07-06 17:23:40 +0530

    Hello Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world! Surya Sample App Rocks!!!!!"
+  end
 end
