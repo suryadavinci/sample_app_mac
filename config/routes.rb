2137be0185a2079be54a652f50be23bbf4338ce4
“Surya <suryadavinci@gmail.com>
2017-07-07 15:28:48 +0530

    updated the routes for fb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'static_pages/fb'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
