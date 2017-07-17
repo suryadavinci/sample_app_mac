ece18b84b56c276936159779698bdb8f409fdd86
Surya <suryadavinci@gmail.com>
2017-07-17 18:12:55 +0530

    Finish user create, edit, index and Destroy actions

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ end
 gem 'rails', '~> 5.1.2'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.11'
+gem 'faker',          '1.7.3'
+gem 'will_paginate',           '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      puts params[:session][:remember_me]
+    #  puts params[:session][:remember_me]
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      redirect_to user
+    #  remember user
+    #  redirect_to user
+    redirect_back_or user
+
     else
       # Create an error message.
       flash.now[:danger] = "Invalid User/Password Combination!!!"
