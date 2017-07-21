5da81c6662fd0c6debc56aea41323cd91db2fe1b
Surya <suryadavinci@gmail.com>
2017-07-21 16:13:30 +0530

    In Progress following, done upto listing 14 19

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :passive_relationships,  class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent: :destroy
+
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
 
 
   attr_accessor :remember_token
@@ -47,4 +54,20 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
